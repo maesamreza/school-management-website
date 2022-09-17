c4926cc6e3052244bf840072a914dfec51eef584
Muhammad Hasnain <92424021+mdhasnainhanif@users.noreply.github.com>
2022-09-17 13:32:14 +0500

    pricing page create

--- hnh-school-management/routes/web.php
+++ hnh-school-management/routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.index');
-});
+Route::get('/', function () { return view('pages.index');});
+Route::get('/pricing', function () { return view('pages.pricing');});
