5f62b2b2a093baa2f927a081fd8ec50c28142b62
Muhammad Hasnain <92424021+mdhasnainhanif@users.noreply.github.com>
2022-09-17 12:51:06 +0500

    first commit

---
+++ hnh-school-management/resources/views/layouts/header.blade.php
@@ -0,0 +1,305 @@
+<header>
+    <div id="theme-menu-one" class="main-header-area pl-100 pr-100">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="col-xl-2 col-lg-2 col-5">
+            <div class="logo">
+              <a href="index.html"
+                ><img src="{{asset('assets/img/logo/header_logo_one.svg')}}" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-xl-7 col-lg-8 d-none d-lg-block">
+            <nav
+              class="main-menu navbar navbar-expand-lg justify-content-center"
+            >
+              <div class="nav-container">
+                <div
+                  class="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+                >
+                  <ul class="navbar-nav">
+                    <li class="nav-item dropdown mega-menu">
+                      <a
+                        class="nav-link drop-icon dropdown-toggle"
+                        href="#"
+                        id="navbarDropdown"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Products
+                      </a>
+                      <ul
+                        class="dropdown-menu submenu mega-menu__sub-menu-box"
+                        aria-labelledby="navbarDropdown"
+                      >
+                        
+                        <div class="container-fluid">
+                          <div class="row">
+                            <div class="col-md-6 col-lg-3">
+                              <div class="pro-card-bg">
+                                <div>
+                                  <i
+                                    class="fa-solid fa-ranking-star category_icon"
+                                  ></i>
+                                </div>
+                                <h6 class="pro-card-heading">
+                                  InstiKit Lite
+                                </h6>
+                                <p
+                                  class="text-gray-500 mt-2 pro-card-text text-sm"
+                                >
+                                  Limited Feature School ERP, Suitable for
+                                  School 100 Students
+                                </p>
+                                <a class="learn-text" href="#">
+                                  <p class="mt-3 learn-text">
+                                    Learn more
+                                    <i class="fa-solid fa-arrow-right"></i>
+                                  </p>
+                                </a>
+                              </div>
+                            </div>
+
+                            <div class="col-md-6 col-lg-3">
+                              <div class="pro-card-bg">
+                                <div>
+                                  <i
+                                    class="fa-solid fa-ranking-star category_icon"
+                                  ></i>
+                                </div>
+                                <h6 class="pro-card-heading">
+                                  InstiKit Lite
+                                </h6>
+                                <p
+                                  class="text-gray-500 mt-2 pro-card-text text-sm"
+                                >
+                                  Limited Feature School ERP, Suitable for
+                                  School 100 Students
+                                </p>
+                                <a class="learn-text" href="#">
+                                  <p class="mt-3 learn-text">
+                                    Learn more
+                                    <i class="fa-solid fa-arrow-right"></i>
+                                  </p>
+                                </a>
+                              </div>
+                            </div>
+
+                            <div class="col-md-6 col-lg-3">
+                              <div class="pro-card-bg">
+                                <div>
+                                  <i
+                                    class="fa-solid fa-ranking-star category_icon"
+                                  ></i>
+                                </div>
+                                <h6 class="pro-card-heading">
+                                  InstiKit Lite
+                                </h6>
+                                <p
+                                  class="text-gray-500 mt-2 pro-card-text text-sm"
+                                >
+                                  Limited Feature School ERP, Suitable for
+                                  School 100 Students
+                                </p>
+                                <a class="learn-text" href="#">
+                                  <p class="mt-3 learn-text">
+                                    Learn more
+                                    <i class="fa-solid fa-arrow-right"></i>
+                                  </p>
+                                </a>
+                              </div>
+                            </div>
+
+                            <div class="col-md-6 col-lg-3">
+                              <div class="pro-card-bg">
+                                <div>
+                                  <i
+                                    class="fa-solid fa-ranking-star category_icon"
+                                  ></i>
+                                </div>
+                                <h6 class="pro-card-heading">
+                                  InstiKit Lite
+                                </h6>
+                                <p
+                                  class="text-gray-500 mt-2 pro-card-text text-sm"
+                                >
+                                  Limited Feature School ERP, Suitable for
+                                  School 100 Students
+                                </p>
+                                <a class="learn-text" href="#">
+                                  <p class="mt-3 learn-text">
+                                    Learn more
+                                    <i class="fa-solid fa-arrow-right"></i>
+                                  </p>
+                                </a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </ul>
+                    </li>
+                    <li class="nav-item active">
+                      <a
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        id="navbarDropdown2"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Pricing
+                      </a>
+                     
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        id="navbarDropdown3"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                        >Feature Request</a
+                      >
+                    
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a
+                        class="nav-link dropdown-toggle text-gray-500"
+                        href="#"
+                        id="navbarDropdown4"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Support
+                      </a>
+                    
+                    </li>
+                    <li class="nav-item">
+                      <a
+                        class="nav-link"
+                        href="contact.html"
+                        id="navbarDropdown5"
+                        role="button"
+                        aria-expanded="false"
+                        >Reseller</a
+                      >
+                    </li>
+                    <li class="nav-item">
+                      <a
+                        class="nav-link"
+                        href="contact.html"
+                        id="navbarDropdown5"
+                        role="button"
+                        aria-expanded="false"
+                        >Contact</a
+                      >
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+          <div class="col-xl-3 col-lg-2 col-7">
+            <div
+              class="right-nav d-flex align-items-center justify-content-end"
+            >
+              <div class="right-btn mr-25 mr-xs-15">
+                <ul class="d-flex align-items-center mt-2">
+                  <li>
+                    <a href="contact.html" class="theme_btn free_btn"
+                      >Register</a
+                    >
+                  </li>
+                  <li>
+                    <a href="contact.html" class="theme_btn free_btn ml-5"
+                      >Login</a
+                    >
+                  </li>
+                </ul>
+              </div>
+              <div
+                class="hamburger-menu d-md-inline-block d-lg-none text-right"
+              >
+                <a href="javascript:void(0);">
+                  <i class="fa-sharp fa-solid fa-bars"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.theme-main-menu -->
+  </header>
+
+
+
+  {{-- mobile nav --}}
+
+
+
+   <!-- slide-bar start -->
+   <aside class="slide-bar">
+    <div class="close-mobile-menu">
+      <a href="javascript:void(0);"><i class="fas fa-times"></i></a>
+    </div>
+
+    <!-- offset-sidebar start -->
+    <div class="offset-sidebar">
+      <div class="offset-widget offset-logo mb-30">
+        <a href="index.html">
+          <img src="{{asset('assets/img/logo/header_logo_one.svg')}}" alt="logo" />
+        </a>
+      </div>
+      <div class="offset-widget mb-40">
+        <div class="info-widget">
+          <h4 class="offset-title mb-20">About Us</h4>
+          <p class="mb-30">
+            But I must explain to you how all this mistaken idea of denouncing
+            pleasure and praising pain was born and will give you a complete
+            account of the system and expound the actual teachings of the
+            great explore
+          </p>
+          <a class="theme_btn theme_btn_bg" href="contact.html">Contact Us</a>
+        </div>
+      </div>
+      <div class="offset-widget mb-30 pr-10">
+        <div class="info-widget info-widget2">
+          <h4 class="offset-title mb-20">Contact Info</h4>
+          <p>
+            <i class="fal fa-address-book"></i> 23/A, Miranda City Likaoli
+            Prikano, Dope
+          </p>
+          <p><i class="fal fa-phone"></i> +0989 7876 9865 9</p>
+          <p><i class="fal fa-envelope-open"></i> info@example.com</p>
+        </div>
+      </div>
+    </div>
+    <!-- offset-sidebar end -->
+
+    <!-- side-mobile-menu start -->
+    <nav class="side-mobile-menu">
+      <ul id="mobile-menu-active">
+        <li class="has-dropdown">
+          <a href="index.html">Products</a>
+          <ul class="sub-menu">
+            <li><a href="#">InstiKit Lite</a></li>
+            <li><a href="#">InstiKit Lite</a></li>
+            <li><a href="#">InstiKit Lite</a></li>
+            <li><a href="#">InstiKit Lite</a></li>
+          </ul>
+        </li>
+        <li><a href="about.html">Pricing</a></li>
+        <li><a href="about.html">Feature Request</a></li>
+        <li><a href="about.html">Support</a></li>
+        <li><a href="about.html">Reseller</a></li>
+        <li><a href="about.html">Contact</a></li>
+      </ul>
+    </nav>
+    <!-- side-mobile-menu end -->
+  </aside>
+
---
+++ hnh-school-management/resources/views/layouts/master.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<!-- Mirrored from html.creativegigstf.com/zoomy/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Sep 2022 10:24:53 GMT -->
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Zoomy - E-learning HTML5 Template</title>
+    <meta name="keywords" content="online education, e-learning, coaching, education, teaching, learning" />
+    <meta name="description"
+        content="Zoomy is a e-learnibg HTML template for all kinds of education, coaching, online education website" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="manifest" href="site.html" />
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png" />
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/font.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/metisMenu.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/spacing.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" />
+</head>
+
+<body>
+    <!--[if lte IE 9]>
+      <p class="browserupgrade">
+        You are using an <strong>outdated</strong> browser. Please
+        <a href="https://browsehappy.com/">upgrade your browser</a> to improve
+        your experience and security.
+      </p>
+    <![endif]-->
+
+    <!-- Add your site or application content here -->
+    <!-- preloader -->
+    <div id="preloader">
+        <div class="preloader">
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <!-- preloader end  -->
+
+
+
+    <div class="body-overlay"></div>
+    <main>
+        @include('layouts.header')
+        @yield('content')
+        @include('layouts.footer')
+    </main>
+
+
+
+
+
+    <!-- JS here -->
+
+    <script src="{{ asset('assets/js/vendor/modernizr-3.5.0.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vendor/jquery-2.2.4.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/slick.min.js') }}"></script>
+    <script src="{{ asset('assets/js/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.nice-select.js') }}"></script>
+    <script src="{{ asset('assets/js/ajax-form.js') }}"></script>
+    <script src="{{ asset('assets/js/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.scrollUp.min.js') }}"></script>
+    <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.easypiechart.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="https://kit.fontawesome.com/335384db5e.js" crossorigin="anonymous"></script>
+</body>
+
+<!-- Mirrored from html.creativegigstf.com/zoomy/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Sep 2022 10:26:14 GMT -->
+
+</html>
